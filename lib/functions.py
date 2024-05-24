0f6da8d01140014df6c9347ef9be2c37c4350957
its-mst <stellaoluoch231@gmail.com>
2024-05-24 08:26:16 -0400

    Running tests

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
+greet("Stella")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
+greet_with_default("Portia")
 
 def add(num1, num2):
-    pass
+    sum=num1+num2
+    return sum
 
 def halve(number):
-    pass
+    division=number/2
+    return division
